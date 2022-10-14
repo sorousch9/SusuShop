3aa02ff15adcfd8957e76201bcee939ca78ce745
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-10-14 20:32:31 +0200

    header updated

--- src/components/Anons/Anons.jsx
+++ src/components/Anons/Anons.jsx
@@ -1,46 +1,52 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./anons.scss";
-import { BsTruck,BsPeople } from "react-icons/bs";
-import { RiArrowGoBackLine,RiMedal2Fill,RiShieldKeyholeLine } from "react-icons/ri";
+import { BsTruck, BsPeople } from "react-icons/bs";
+import {
+  RiArrowGoBackLine,
+  RiMedal2Fill,
+  RiShieldKeyholeLine,
+} from "react-icons/ri";
 export const Anons = () => {
   return (
     <Container className="container">
-      <div className="anons-info">
-        <p>
-          über 7 Mio. Kunden{" "}
-          <span>
-            <BsPeople />
-          </span>
-        </p>
+      <Row>
+        <div className="anons-info">
+          <p>
+            über 7 Mio. Kunden{" "}
+            <span>
+              <BsPeople />
+            </span>
+          </p>
 
-        <p>
-          24h-Versand{" "}
-          <span>
-            <BsTruck />
-          </span>
-        </p>
+          <p>
+            24h-Versand{" "}
+            <span>
+              <BsTruck />
+            </span>
+          </p>
 
-        <p>
-          Rücksendung kostenlos{" "}
-          <span>
-            <RiArrowGoBackLine />
-          </span>
-        </p>
+          <p>
+            Rücksendung kostenlos{" "}
+            <span>
+              <RiArrowGoBackLine />
+            </span>
+          </p>
 
-        <p>
-          über 200.000 Artikel{" "}
-          <span>
-            <RiMedal2Fill />
-          </span>
-        </p>
+          <p>
+            über 200.000 Artikel{" "}
+            <span>
+              <RiMedal2Fill />
+            </span>
+          </p>
 
-        <p>
-          Sicher einkaufen{" "}
-          <span>
-            <RiShieldKeyholeLine />
-          </span>
-        </p>
-      </div>
+          <p>
+            Sicher einkaufen{" "}
+            <span>
+              <RiShieldKeyholeLine />
+            </span>
+          </p>
+        </div>
+      </Row>
     </Container>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,11 @@
 import "./header.scss";
-import logo from "../../assets/LOGO.png";
-import React from "react";
-import usericon from "../../assets/icon-user.svg";
-import carticon from "../../assets/icon-cart.svg";
-import hearticon from "../../assets/icon-heart.svg";
 import { Link } from "react-router-dom";
+import logo from "../../assets/LOGO.png";
 import { Col, Container, Row } from "react-bootstrap";
 import { Badge } from "@mui/material";
-
+import { BsSearch, BsCardChecklist } from "react-icons/bs";
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
+import { IoBusinessSharp } from "react-icons/io5";
 export const Header = () => {
   return (
     <Container>
@@ -19,11 +17,11 @@ export const Header = () => {
             </a>
           </div>
         </Col>
-        <Col md="10">
-          <div class="header-right">
-            <div class="search-style">
+        <Col md="5">
+          <div className="header-right">
+            <div className="search-style">
               <form>
-                <select class="select-active">
+                <select className="select-active">
                   <option>All Categories</option>
                   <option>Women's</option>
                   <option>Men's</option>
@@ -36,149 +34,169 @@ export const Header = () => {
                   <option>Shoes</option>
                   <option>Mother & Kids</option>
                 </select>
-                <input type="text" placeholder="Search" value="" />
+                <div className="input-item">
+                  <i>
+                    <BsSearch />
+                  </i>
+                  <input type="text" placeholder="Wonach suchen Sie ?" />
+                </div>
               </form>
             </div>
-            <div class="header-action-right">
-              <div className="header-action">
-                <div className="header-action-icon">
-                  <Link
-                    to="/"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                    className="text-link"
+          </div>
+        </Col>
+        <Col md="3">
+          <div class="header-action-right">
+            <div className="header-action">
+              <div className="header-action-icon">
+                <Link
+                  to="/"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                  className="text-link"
+                >
+                  <Badge
+                    badgeContent={4}
+                    color="success"
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
                   >
-                    <Badge
-                      badgeContent={4}
-                      color="success"
-                      anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "left",
-                      }}
-                    >
-                      <img className="svgInject" alt="Nest" src={carticon} />
-                    </Badge>
-                    <span>Warenkorb</span>
-                  </Link>
-                </div>
-                <div className="header-action-icon">
-                  <Link
-                    to="/"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                    className="text-link"
+                    <AiOutlineShoppingCart
+                      style={{ fontSize: "1.3rem", marginRight: "5px" }}
+                    />
+                  </Badge>
+                  <span>Warenkorb</span>
+                </Link>
+              </div>
+              <div className="header-action-icon">
+                <Link
+                  to="/"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                  className="text-link"
+                >
+                  <Badge
+                    badgeContent={4}
+                    color="success"
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
                   >
-                    <Badge
-                      badgeContent={4}
-                      color="success"
-                      anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "left",
-                      }}
-                    >
-                      <img className="svgInject" alt="Nest" src={hearticon} />
-                    </Badge>
-                    <span>Merkzettel</span>
-                  </Link>
-                </div>
+                    <BsCardChecklist
+                      style={{ fontSize: "1.3rem", marginRight: "5px" }}
+                    />
+                  </Badge>
 
-                <div className="header-action-icon">
-                  <Link
-                    to="/"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                    className="text-link"
-                  >
-                    <img className="svgInject" alt="Nest" src={usericon} />
-                    <span>Account</span>
-                  </Link>
+                  <span>Einkaufslisten</span>
+                </Link>
+              </div>
+              <div className="header-action-icon">
+                <Link
+                  to="/"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                  className="text-link"
+                >
+                  <AiOutlineUser
+                    style={{ fontSize: "1.3rem", marginRight: "5px" }}
+                  />
+                  <span>Account</span>
+                </Link>
 
-                  <div className="cart-dropdown-wrap">
-                    <ul>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-user mr-10"></i>
-                          My Account
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-location-alt mr-10"></i>
-                          Order Tracking
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-label mr-10"></i>
-                          My Voucher
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-heart mr-10"></i>
-                          My Wishlist
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-settings-sliders mr-10"></i>
-                          Setting
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          style={{
-                            color: "inherit",
-                            textDecoration: "inherit",
-                          }}
-                          className="text-link"
-                        >
-                          <i className="fi fi-rs-sign-out mr-10"></i>
-                          Sign out
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
+                <div className="cart-dropdown-wrap">
+                  <ul>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-user mr-10"></i>
+                        My Account
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-location-alt mr-10"></i>
+                        Order Tracking
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-label mr-10"></i>
+                        My Voucher
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-heart mr-10"></i>
+                        My Wishlist
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-settings-sliders mr-10"></i>
+                        Setting
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/"
+                        style={{
+                          color: "inherit",
+                          textDecoration: "inherit",
+                        }}
+                        className="text-link"
+                      >
+                        <i className="fi fi-rs-sign-out mr-10"></i>
+                        Sign out
+                      </Link>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
           </div>
         </Col>
+        <Col md="2">
+          <div className="hotline">
+            <IoBusinessSharp style={{ fontSize: "2.4rem",color:"#474747",marginRight:"5px" }} />
+            <p>
+            Geschäftskundenshop<span>Alle Preise werden dann<br></br> zzgl. MwSt. angezeigt.</span>
+            </p>
+          </div>
+        </Col>
       </Row>
-  
     </Container>
   );
 };
