3ea7f051ecfa4b03d3fd8051359b648738b94a51
Soroush Safarkhah <soroush_Raw@hotmail.com>
2022-10-21 19:29:27 +0200

    spacial offer components created

--- src/components/Main/SpecialOffer/SpecialOffer.jsx
+++ src/components/Main/SpecialOffer/SpecialOffer.jsx
@@ -7,7 +7,7 @@ export const SpecialOffer = () => {
   return (
     <div className="spacial-offer">
       <Row>
-        <div className="header-so">
+        <div className="module-header">
           <Col xl="6">
             <div className="column">
               <h4>
@@ -20,7 +20,7 @@ export const SpecialOffer = () => {
             </div>
           </Col>
           <Col xl="6">
-            <div className="column1">React CountDown</div>
+            <div className="column-countdown">React CountDown</div>
           </Col>
         </div>
       </Row>
@@ -31,7 +31,9 @@ export const SpecialOffer = () => {
             <div className="thumbnail-wrapper">
               <div className="product-badges">
                 <span className="badge style-1 onsale">20%</span>
-                <span className="badge style-2 organic">Sofort lieferbar</span>
+                <span className="badge style-2 consistent">
+                  Sofort lieferbar
+                </span>
               </div>
               <Link
                 to="/"
@@ -48,7 +50,7 @@ export const SpecialOffer = () => {
                     style={{ color: "inherit", textDecoration: "inherit" }}
                   />
                 </Link>
-                <div className="product-buttons">
+                
                   <Link
                     to="/"
                     className="detail-bnt quick-view-button"
@@ -56,41 +58,25 @@ export const SpecialOffer = () => {
                   >
                     <BsHeart />
                   </Link>
-                  <div className="tinv-wraper tinv-wishlist tinvwl-shortcode-add-to-cart">
-                    <div className="tinv-wishlist-clear"></div>
-                    <Link
-                      to="-"
-                      style={{ color: "inherit", textDecoration: "inherit" }}
-                    >
-                      <span className="tinvwl_add_to_wishlist-text">
-                        Add to Wishlist
-                      </span>
-                    </Link>
-                    <div className="tinv-wishlist-clear"></div>{" "}
-                    <div className="tinvwl-tooltip">Add to Wishlist</div>
-                  </div>
-                </div>
+                
               </div>
+              
             </div>
             <div className="content-wrapper">
               <span className="price">
                 <del aria-hidden="true">
                   <span className="amount">
                     <bdi>
-                      <span className="">
-                        
-                      </span>
-                      24.00€
+                      <span className=""></span>
+                      240.00€
                     </bdi>
                   </span>
                 </del>{" "}
                 <ins>
                   <span className="amount">
                     <bdi>
-                      <span >
-                      €
-                      </span>
-                      19.50
+                      <span>€</span>
+                      190.50
                     </bdi>
                   </span>
                 </ins>
@@ -113,60 +99,29 @@ export const SpecialOffer = () => {
                   role="img"
                   aria-label="Rated 5.00 out of 5"
                 >
-                  <span style={{ width: "100%" }}>
-                   
-                  </span>
+                  <span style={{ width: "100%" }}></span>
                 </div>
                 <div className="count-rating">
                   58 <span className="rating-text">Ratings</span>
                 </div>
               </div>
               <div className="product-count">
-                <div className="product-progress">
-                  <span className="progress" style={{ width: "95%" }}></span>
-                </div>
+                
                 <div className="product-pcs">
-                die verfügbaren produkten : <span>26</span>
+                  die verfügbaren produkten : <span>26</span>
                 </div>
               </div>
               <div className="product-fade-block">
-                <div className="product-button-group cart-with-quantity ">
-                  <div className="quantity ajax-quantity">
-                    <div className="quantity-button minus">
-                      <i className="klbth-icon-minus"></i>
-                    </div>
-                    <input
-                      type="text"
-                      className="input-text qty text"
-                      name="quantity"
-                      step="1"
-                      min=""
-                      max="26"
-                      defaultValue="1"
-                      title="Menge"
-                      size="4"
-                      inputMode="numeric"
-                    />
-                    <div className="quantity-button plus">
-                      <i className="klbth-icon-plus"></i>
-                    </div>
-                  </div>
-                  <Link
-                    to="/"
-                    style={{ color: "inherit", textDecoration: "inherit" }}
-                  >
-                    In den Warenkorb
-                  </Link>
-                </div>
+                <button className="product-btn-fade">In den Warenkorb</button>
               </div>
             </div>
           </div>
-          <div
-            className="product-content-fade border-info"
-            style={{ marginBottom: "-49px" }}
-          ></div>
-          <div className="product-content-fade"></div>
         </div>
+        <div
+          className="product-content-fade border-info"
+          style={{ marginBottom: "-49px" }}
+        ></div>
+        <div className="product-content-fade"></div>
       </div>
     </div>
   );
