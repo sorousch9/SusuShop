542428cc38b37f731a0950349f5a376ca22faafb
Sorousch9 <soroush.raw@gmail.com>
2022-11-03 17:19:37 +0100

    cart page styled

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -6,6 +6,7 @@ import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/Footer/Footer";
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import p1 from "../../assets/products-img/bohrschrauber.png";
 
 export const Cart = () => {
   return (
@@ -31,32 +32,33 @@ export const Cart = () => {
                 <form className="cart-form">
                   <div className="free-progress-bar">
                     <div className="free-shipping-notice">
-                      <span className="price-amount">€ *5</span>
+                      Nur
+                      <span className="price-amount"> 5€ </span>
                       in den Warenkorb legen und versandkostenfrei erhalten!
                     </div>
                     <div className="raw-progress-bar">
                       <span
                         className="progress"
-                        style={{ width: "84%" }}
+                        style={{ width: "85%" }}
                       ></span>
                     </div>
                   </div>
                   <table className="table">
                     <thead>
                       <tr>
-                        <th className="product-thumbnail">&nbsp;</th>
-                        <th className="product-name">Produkt</th>
-                        <th className="product-price">Einzelpreis</th>
-                        <th className="product-quantity">Menge</th>
-                        <th className="product-subtotal">Gesamtpreis</th>
-                        <th className="product-remove">&nbsp;</th>
+                        <th>&nbsp;</th>
+                        <th>Produkt</th>
+                        <th>Einzelpreis</th>
+                        <th>Menge</th>
+                        <th>Gesamtpreis</th>
+                        <th>&nbsp;</th>
                       </tr>
                     </thead>
                     <tbody>
                       <tr className="cart-item">
                         <td className="product-thumbnail">
                           <Link to={"/"} target="_blank">
-                            <img className="product-image" src={""} alt="" />
+                            <img className="product-image" src={p1} alt="" />
                           </Link>
                         </td>
                         <td className="product-name">
@@ -67,11 +69,15 @@ export const Cart = () => {
                         <td className="product-price">178.22 €</td>
                         <td className="product-quantity">
                           <div className="quantity">
-                            <div className="quantity-btn minus">
+                            <div className="quantity-btn">
                               <span className="icon-minus"></span>
                             </div>
-                            <input className="text-input" type="text" />
-                            <div className="quantity-btn plus">
+                            <input
+                              className="text-input"
+                              type="text"
+                              defaultValue={1}
+                            />
+                            <div className="quantity-btn">
                               <span className="icon-plus"></span>
                             </div>
                           </div>
@@ -83,30 +89,75 @@ export const Cart = () => {
                           </Link>
                         </td>
                       </tr>
-
-                      <tr className="cart-coupon">
-                        <td className="actions">
-                          <div className="actions-wrapper">
-                            <div className="coupon">
-                              <input
-                                type="text"
-                                className="input-text"
-                                placeholder="Gutscheincode"
-                              />
-                              <button type="submit" className="coupon-btn">
-                                Gutschein anwenden
-                              </button>
-                            </div>
-                            <button type="submit" className="coupon-btn">
-                              Warenkorb aktualisieren
-                            </button>
-                          </div>
-                        </td>
-                      </tr>
                     </tbody>
                   </table>
+                  <div className="actions">
+                    <div className="actions-wrapper">
+                      <div className="coupon">
+                        <input
+                          type="text"
+                          className="input-text"
+                          placeholder="Gutscheincode"
+                        />
+                        <button type="submit" className="coupon-btn">
+                          Gutschein anwenden
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </form>
-                <div className="cart-info"></div>
+                <div className="cart-info">
+                  <div className="cart-totals">
+                    <h2>Gesamtsumme</h2>
+                    <table cellSpacing={0} className="shop-info-table">
+                      <tbody>
+                        <tr className="cart-subtotal">
+                          <th>Zwischensumme</th>
+                          <td className="cart-subtotal-item"> 178 €</td>
+                        </tr>
+                        <tr className="cart-shopping-totals">
+                          <th>Versand</th>
+                          <td className="shopping">
+                            <ul>
+                              <li>
+                                <input
+                                  type="radio"
+                                  className="shipping-method"
+                                  defaultChecked
+                                />
+                                <label
+                                  className="shipping-label"
+                                >
+                                  Flatrate: <span>3.50 €</span>
+                                </label>
+                              </li>
+                              <li>
+                                <input
+                                  type="radio"
+                                  className="shipping-method"
+                                />
+                                <label
+                                  className="shipping-label"
+                                >
+                                  Lokale Aufnahme
+                                </label>
+                              </li>
+                            </ul>
+                          </td>
+                        </tr>
+                        <tr className="cart-order-total">
+                          <th>Gesamtsumme</th>
+                          <td>178 €</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div className="checkout">
+                      <Link to={"/checkout"}>
+                        <button className="to-checkout">Checkout</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
               </div>
             </Col>
           </Row>
